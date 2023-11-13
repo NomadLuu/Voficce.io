b2644c1dbf69301f7ca7e2569e4a56e4ceaf541f
NomadLuu <119811501+NomadLuu@users.noreply.github.com>
2023-11-13 01:23:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const typed = new Typed('.typed',{
+    stringsElement:'#cadenas-texto',
+    typeSpeed:75,
+    backSpeed:75,
+    startDelay:300,
+    smartBackspace:true,
+    loop:true,
+    loopCount:false,
+    showCursor:true,
+    cursorChar:'|',
+    contentType:'html',
+});
